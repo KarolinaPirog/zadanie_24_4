3a2380c27178aad4719dd060af157b2f83ceae13
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-13 14:18:52 +0200

    add time formatting

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const formatDate = (timeInSeconds) => {
-    return `${timeInSeconds}s`;
-  }
+const formatDate = (secondsOS) => {
+    if (secondsOS >= 0) { 
+        var hours = Math.floor((secondsOS/3600)).toFixed(0);
+        var minutes = Math.floor((secondsOS - (hours * 3600))/60).toFixed(0);
+        var seconds = Math.floor(secondsOS - (hours * 3600) - (minutes*60)).toFixed(0);
+
+        if (hours == 0) {
+            if (minutes == 0) {
+                return seconds + " sec.";
+            } else {
+                return minutes + " min. " + seconds + " sec.";
+            };
+        } else {
+            return hours + " h. " + minutes + " min. " + seconds + " sec."; 
+        };
+    } else {
+        return "Value should be greater than 0.";
+    };
+};
   
   module.exports = formatDate;
